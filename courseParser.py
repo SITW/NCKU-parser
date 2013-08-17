8cc799498fac517063929492aeca603a807ad315
Gary Lai <garylai1990@gmail.com>
2013-08-18 01:52:16 +0800

    Create method which will auto create a result directory if the result dir doesn't exist

--- courseParser.py
+++ courseParser.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import urllib
+import urllib, HTMLParser, os, errno
 
 #courseWeb = urllib.urlopen("http://140.116.165.74/qry/qry001.php?dept_no="+"H3")
 #webContent = courseWeb.read().decode('utf_8')
 #courseWeb.close()
 
-import HTMLParser
 
 added = False
 formStart = False
@@ -15,6 +14,8 @@ result = []
 course = []
 
 title = [ "系所名稱" , "系號" , "序號" , "課程碼" , "分班碼" , "班別" , "年級" , "類別" , "英語授課" , "課程名稱 (連結課程地圖)" , "選必修" , "學分" , "教師姓名 *:主負責老師" , "已選課人數 " , "餘額 " , "時間" , "教室" , "備註" , "限選條件" , "業界專家參與" , "屬性碼" , "跨領域學分學程" ]
+
+departNum=["A2","A3","A4","A5","A6","AA","AH","AN","C0","XZ","A1","A7","A8","A9","AG","B1","K1","B2","K2","B3","K3","B5","K5","K4","C1","L1","C2","L2","C3","L3","C4","L4","F8","L7","LA","VF","E0","E1","N1","E3","N3","E4","N4","E5","N5","E6","N6","E8","N8","E9","N9","F0","F1","P1","F4","P4","F5","P5","F6","P6","F9","P8","N0","NA","NB","NC","Q4","H1","R1","H2","R2","H3","R3","H4","R4","H5","R5","R0","R6","R7","R8","R9","RA","RB","RC","RD","RZ","I2","T2","I3","T3","I5","I6","T6","I7","T7","S0","S1","S2","S3","S4","S5","S6","S7","S8","S9","T1","T4","T8","T9","TA","TB","TC","D2","U2","U6","D4","D5","U5","D8","U7","U1","U3","E2","N2","F7","P7","ND","P9","Q1","Q3","Q5","Q6","V1","V2","V3","V4","V5","V6","V7","V8","V9","VA","VB","VC","VD","VE","VG","VH","VJ","VK","E7","N7","F2","P2","F3","P3","PA","C5","L5","L6","L8","Z0","Z1","Z2","Z3","A2","A3","A4","A5","A6","AA","AH","AN","C0","XZ","A1","A7","A8","A9","AG","B1","K1","B2","K2","B3","K3","B5","K5","K4","C1","L1","C2","L2","C3","L3","C4","L4","F8","L7","LA","VF","E0","E1","N1","E3","N3","E4","N4","E5","N5","E6","N6","E8","N8","E9","N9","F0","F1","P1","F4","P4","F5","P5","F6","P6","F9","P8","N0","NA","NB","NC","Q4","H1","R1","H2","R2","H3","R3","H4","R4","H5","R5","R0","R6","R7","R8","R9","RA","RB","RC","RD","RZ","I2","T2","I3","T3","I5","I6","T6","I7","T7","S0","S1","S2","S3","S4","S5","S6","S7","S8","S9","T1","T4","T8","T9","TA","TB","TC","D2","U2","U6","D4","D5","U5","D8","U7","U1","U3","E2","N2","F7","P7","ND","P9","Q1","Q3","Q5","Q6","V1","V2","V3","V4","V5","V6","V7","V8","V9","VA","VB","VC","VD","VE","VG","VH","VJ","VK","E7","N7","F2","P2","F3","P3","PA","C5","L5","L6","L8","Z0","Z1","Z2","Z3"]
 
 class courseHTMLParser(HTMLParser.HTMLParser):
 
@@ -75,6 +76,7 @@ def mainProcess(fileName):
   Parser.close()
   result[0] = title
 
+  mkdir_p("./result")
   inputFile=open("./result/"+ fileName+".json" , 'w' )
 
   inputFile.write("[\n")
@@ -91,10 +93,15 @@ def mainProcess(fileName):
       inputFile.write( ",\n")
   inputFile.write("\n]")
 
+def mkdir_p(path):
+  try:
+    os.makedirs(path)
+  except OSError as exc: # Python >2.5
+    if exc.errno == errno.EEXIST and os.path.isdir(path):
+      pass
+    else: raise
 
-d=["A2","A3","A4","A5","A6","AA","AH","AN","C0","XZ","A1","A7","A8","A9","AG","B1","K1","B2","K2","B3","K3","B5","K5","K4","C1","L1","C2","L2","C3","L3","C4","L4","F8","L7","LA","VF","E0","E1","N1","E3","N3","E4","N4","E5","N5","E6","N6","E8","N8","E9","N9","F0","F1","P1","F4","P4","F5","P5","F6","P6","F9","P8","N0","NA","NB","NC","Q4","H1","R1","H2","R2","H3","R3","H4","R4","H5","R5","R0","R6","R7","R8","R9","RA","RB","RC","RD","RZ","I2","T2","I3","T3","I5","I6","T6","I7","T7","S0","S1","S2","S3","S4","S5","S6","S7","S8","S9","T1","T4","T8","T9","TA","TB","TC","D2","U2","U6","D4","D5","U5","D8","U7","U1","U3","E2","N2","F7","P7","ND","P9","Q1","Q3","Q5","Q6","V1","V2","V3","V4","V5","V6","V7","V8","V9","VA","VB","VC","VD","VE","VG","VH","VJ","VK","E7","N7","F2","P2","F3","P3","PA","C5","L5","L6","L8","Z0","Z1","Z2","Z3","A2","A3","A4","A5","A6","AA","AH","AN","C0","XZ","A1","A7","A8","A9","AG","B1","K1","B2","K2","B3","K3","B5","K5","K4","C1","L1","C2","L2","C3","L3","C4","L4","F8","L7","LA","VF","E0","E1","N1","E3","N3","E4","N4","E5","N5","E6","N6","E8","N8","E9","N9","F0","F1","P1","F4","P4","F5","P5","F6","P6","F9","P8","N0","NA","NB","NC","Q4","H1","R1","H2","R2","H3","R3","H4","R4","H5","R5","R0","R6","R7","R8","R9","RA","RB","RC","RD","RZ","I2","T2","I3","T3","I5","I6","T6","I7","T7","S0","S1","S2","S3","S4","S5","S6","S7","S8","S9","T1","T4","T8","T9","TA","TB","TC","D2","U2","U6","D4","D5","U5","D8","U7","U1","U3","E2","N2","F7","P7","ND","P9","Q1","Q3","Q5","Q6","V1","V2","V3","V4","V5","V6","V7","V8","V9","VA","VB","VC","VD","VE","VG","VH","VJ","VK","E7","N7","F2","P2","F3","P3","PA","C5","L5","L6","L8","Z0","Z1","Z2","Z3"]
-
-for i in range(len(d)):
+for i in range(len(departNum)):
 
   added = False
   formStart = False
@@ -102,6 +109,6 @@ for i in range(len(d)):
   result = []
   course = []
 
-  if(d[i]=="RZ"):
+  if(departNum[i]=="RZ"):
     continue
-  mainProcess(d[i])
+  mainProcess(departNum[i])
