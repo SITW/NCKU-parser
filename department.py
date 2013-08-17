1cc10d75d126c0f98fdc5149b0fec2a7354f6637
Gary Lai <garylai1990@gmail.com>
2013-08-18 01:26:33 +0800

    清除所有結尾的空白，把所有的縮排設定成空白，http://stackoverflow.com/questions/119562/tabs-versus-spaces-in-python-programming

--- department.py
+++ department.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-import urllib 
+import urllib
 
 courseIndex = urllib.urlopen("http://140.116.165.74/qry/index.php")
 webContent = courseIndex.read().decode('utf_8')
@@ -13,23 +13,22 @@ departmentName = []
 
 class departmentHTMLParser(HTMLParser.HTMLParser):
 
-	def handle_data(self,data):
-		if data.find('( ') >= 0:
-#			print "%s" %data[3:5]
-			departmentNo.append(data[3:5])
-			departmentName.append(data[7:])
+  def handle_data(self,data):
+    if data.find('( ') >= 0:
+#     print "%s" %data[3:5]
+      departmentNo.append(data[3:5])
+      departmentName.append(data[7:])
 
 Parser = departmentHTMLParser()
 
 try:
-	for line in webContent.splitlines():
-		if hasattr(Parser,'stop') and Parser.stop:
-			break
-		Parser.feed(line)
+  for line in webContent.splitlines():
+    if hasattr(Parser,'stop') and Parser.stop:
+      break
+    Parser.feed(line)
 except HTMLParser.HTMLParseError,data:
-	print "error" + data.msh
+  print "error" + data.msh
 Parser.close()
 
 for i in range(len(departmentName)):
-	print departmentName[i]
-
+  print departmentName[i]
