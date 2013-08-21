5c8a6e06f0fe670481178f777efcd7fc8ea6826c
Gary Lai <garylai1990@gmail.com>
2013-08-21 20:11:04 +0800

    add progress bar

--- club-parser/club_parser.rb
+++ club-parser/club_parser.rb
@@ -3,6 +3,9 @@
 require 'nokogiri'
 require 'open-uri'
 require 'json'
+require 'ruby-progressbar'
+
+progressbar = ProgressBar.create(:format => '%a |%b>>%i| %p%% %t')
 
 club_url = Nokogiri::HTML(open('http://mis.osa.ncku.edu.tw/club2/test02/assquery.php'))
 
@@ -20,19 +23,21 @@ def parse_club_to_json(key)
   return info
 end
 
-club_url.css('//#man_list_2/option').each do |p|
+club_url.css('//#man_list_2/option').each_with_index do |p, i|
   club_id = p["value"]
   club_name = p.content
   club_data[club_id] = club_name
+  progressbar.total = i+1
 end
 
 
 club_data.each do |key, value|
-  puts "Parsing " + club_data[key]
+  # puts "Parsing " + club_data[key]
+  progressbar.increment
   club_info << parse_club_to_json(key)
 end
 
-result_path = '../club_result'
+result_path = '../club-result'
 Dir.mkdir(result_path) unless File.directory? result_path
 begin
   File.open(result_path + "/club_result.json","w") do |f|
